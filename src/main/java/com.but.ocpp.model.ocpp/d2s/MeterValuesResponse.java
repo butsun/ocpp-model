87fa5cd0d6032e5017d5f6db9663c0105cfdcceb
jun.sun <jun.sun1@cn.abb.com>
2023-11-22 11:00:31 +0800

    feat(d2s): 补充缺少的meterValueResponse

---
+++ src/main/java/com.but.ocpp.model.ocpp/d2s/MeterValuesResponse.java
@@ -0,0 +1,15 @@
+package com.but.ocpp.model.ocpp.d2s;
+
+import com.but.ocpp.model.ocpp.ResponseType;
+
+/**
+ * @Author jun.sun
+ * @ClassName MeterValuesResponse
+ * @Description TODO
+ * @Date 2023/11/22 10:59
+ * @Version 1.0
+ */
+public class MeterValuesResponse implements ResponseType {
+
+
+}
